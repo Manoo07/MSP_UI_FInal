d5f3ac8ea6ff768d1ad860a3830d5cc43e06484a
Manoo07 <manoharboinapalli1196@gmail.com>
2024-03-19 11:04:26 +0530

    Added the loading

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = () => {
   return (
     <div className="loader-container flex flex-col gap-2">
       <div className="loader"></div>
-      <div>Generating Email...</div>
+      <div>loading...</div>
     </div>
   );
 };
